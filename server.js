680a77d0d60186ff4636fe4066d0dd0f61c244d5
James T Ardian <jamesardian01@gmail.com>
2021-12-29 10:59:58 -0800

    changed express session settings to get it to set up cookie

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
+app.enable('trust proxy');
 app.use(expressSession({
     key: "username",
     secret: "doesnt actually matter",
